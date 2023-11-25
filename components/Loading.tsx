73d1f32dfd057a5339b8a0f4dae98b1616da7312
Sumit <sk990005@outlook.com>
2023-11-25 09:14:05 +0530

    implement product listing

---
+++ components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+	return (
+		<div className="min-h-screen min-w-screen flex-center">
+			<p>Loading....</p>
+		</div>
+	);
+};
+
+export default Loading;
